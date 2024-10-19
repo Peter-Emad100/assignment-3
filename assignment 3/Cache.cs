19f712a1dfaca1eda3e321afeff9b043d58bf1fe
Peter Emad <139802433+Peter-Emad100@users.noreply.github.com>
2024-10-19 20:35:29 +0300

    Add project files.

---
+++ assignment 3/Cache.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_3
+{
+    public class Cache<Tx, Ty>:IEnumerable where Tx : IComparable<Tx> where Ty :IComparable<Ty>
+    {
+
+        private Tx[] tx= new Tx[15];
+        private Ty[] ty = new Ty[15];
+        private int[] freq = new int[15];
+        int index = 0;
+        public void add(Tx tx1,Ty ty2)
+        {
+            if (index == 15)
+            {
+                int temp = Array.IndexOf(freq, freq.Min());
+                tx[temp] = tx1;
+                ty[temp] = ty2;
+            }
+            else
+            {
+                tx[index] = tx1;
+                ty[index] = ty2;
+                index++;
+            }
+        }
+        public Ty retrieve(Tx tx1)
+        {
+            for (int i = 0; i <= index; i++)
+            {
+                if (tx[i].CompareTo(tx1)==0)
+                {
+                    return ty[i];
+                }
+            }
+            return (default);
+        }
+        public void delete(Tx tx1)
+        {
+            for (int i = 0; i < index; i++)
+            {
+                if (tx[i].CompareTo(tx1) == 0)
+                {
+                    tx[i] = default;
+                    ty[i] = default;
+                    freq[i] = -1;
+                }
+            }
+        }
+        public Tx getValueByindex(int ind)
+        {
+            return tx[ind];
+        }
+        public Ty getKeyByindex(int ind)
+        {
+            return ty[ind];
+        }
+        public IEnumerator GetEnumerator() => new InternalCache(this);
+        public class InternalCache:IEnumerator
+        {
+            int innerIndex = -1;
+            Cache<Tx, Ty> cac;
+            public InternalCache(Cache<Tx,Ty> ca) {
+                cac = ca;
+            }
+            
+            public bool MoveNext()
+            {
+                while (++innerIndex < cac.index && cac.tx[innerIndex].CompareTo(default) == 0) ;
+                
+                return innerIndex <cac.index;
+            }
+            public void Reset()
+            {
+                innerIndex = -1;
+            }
+            public object Current
+            {
+                get
+                {
+                        return $"tkey          {cac.tx[innerIndex]} tvalue        {cac.ty[innerIndex]}";
+                }
+            }
+        }
+    }
+}
---
+++ assignment 3/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace assignment_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cache<char,int> cha = new Cache<char,int>();
+            cha.add('a', 10);
+            cha.add('b', 10); 
+            cha.add('c', 10);
+            cha.add('d', 10);
+            foreach(string item in cha)
+            {
+                Console.WriteLine(item);
+            }
+           /* int[] numbers = { 1, 2, 3, 4, 5, 6, 432, 4 };
+            Console.WriteLine(FindMax(numbers));
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }*/
+        }
+        static void ReverseArray<T>(T[] array)
+        {
+            T temp;
+            int len=array.Length;
+           for (int i = 0; i < len/2; i++)
+            {
+                temp = array[i];
+                array[i] = array[len - 1 - i];
+                array[len - 1 - i] = temp;
+            }
+        }
+        static T FindMax<T>(T[] array) where T : IComparable<T>
+        {
+            T max = array[0];
+            int result;
+            for(int i = 1; i < array.Length; i++)
+            {
+                result = max.CompareTo(array[i]);
+                if (result < 0)
+                {
+                    max= array[i];
+                }
+            }
+            return max;
+        }
+    }
+}
